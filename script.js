17baa4df6f0b8c688e2a3c4b5bf6ee3300283a33
vicdd <victorrobcam@gmail.com>
2023-08-12 09:38:14 -0300

    add primary js functions

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 let myLibrary = [];
 
-function Book() {
-  // the constructor...
+class Book {
+    constructor(id, author, title, pages, read) {
+        this.id = id;
+        this.author = author;
+        this.title = title;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(book) {
+    myLibrary.append(book);
+}
+
+function removeBookFromLibrary(id) {
+    let idx = myLibrary.map(e => e.id).indexOf(id);
+    myLibrary.splice(idx, 1);
+}
+
+function showLibrary() {
+    for (let i = 0, n = myLibrary.length; i < n; i++) {
+        console.log(myLibrary[i]);
+    }
+}
+
+function changeReadStatus(id) {
+    let idx = myLibrary.map(e => e.id).indexOf(id);
+    myLibrary[idx].read = !myLibrary[idx].read;
 }
